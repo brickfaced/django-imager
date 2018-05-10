bf72d4ca87398b48e37cf57f9f8bbeeffbbb9220
brickfaced <brickfazed@hotmail.com>
2018-05-10 16:34:54 -0700

    Added PhotoDetailView class

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PhotosView, AlbumsView, LibraryView, PhotoView
+from .views import PhotosView, AlbumsView, LibraryView, PhotoDetailView
 
 urlpatterns = [
     path('photos', PhotosView.as_view(), name='photos'),
     path('albums', AlbumsView.as_view(), name='albums'),
     path('library', LibraryView.as_view(), name='library'),
-    path('photos/<photo_id>', PhotoView.as_view(), name='single_photo'),
+    path('photos/<photo_id>', PhotoDetailView.as_view(), name='single_photo'),
+    # path('photos/<photo_id>/edit', PhotoView.as_view(), name='single_photo'),
     # path('albums/<str:album_id>', album_view, name='album'),
 ]
